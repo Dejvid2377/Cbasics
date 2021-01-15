f15dda25eae3fc401ed88a01003837b46a2f8610
Dejvid238 <dawidkrekora@gmail.com>
2021-01-15 10:56:36 +0100

    plik naglowkowy zawierajacy glowne struktury zawarte w funkcjach

---
+++ cwiczenia/lista4/inc/zmienne_pomocnicze.h
@@ -0,0 +1,12 @@
+#ifndef ZMIENNE_POMOCNICZE_H
+#define ZMIENNE_POMOCNICZE_H
+
+#define MAX 5       //rozmiar boku kwadratu na ktorym znajduje sie pole gry
+                    //mozna modyfikowac wedle potrzeby
+
+typedef struct {    //tworzenie struktury okreslajacej wspolrzedne pola na tablicy do gry
+ int x;
+ int y;
+}wspolrzedne;
+
+#endif
